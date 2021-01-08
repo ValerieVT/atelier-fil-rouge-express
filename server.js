a8bfa23f5fe4db76d2abaa41c03124fe77cf403d
Valerie TV <vtylskivincent@gmail.com>
2021-01-08 17:30:49 +0100

    PUT road added

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const pool = require('./pool');
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(express.json());
+
 app.get('/api/recoltes/', (req, res) => {
   pool.query('SELECT * FROM recolte', (err, results) => {
     if (err) {
@@ -136,6 +138,65 @@ app.get('/api/recoltes/search', (req, res) => {
       res.status(200).json(results[0]);
     } else {
       res.status(200).json(results);
+    }
+  });
+});
+
+// entrée d'une nouvelle récolte
+app.post('/api/recoltes', (req, res) => {
+  pool.query('INSERT INTO recolte (vegetable, date, overage, weight_in_gramms) VALUES (?, ?, ?, ?)', [req.body.vegetable, req.body.date, req.body.overage, req.body.weight_in_gramms], (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+      })
+    } else {
+      return pool.query('SELECT * FROM recolte WHERE id = ?', results.insertId, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });
+        }
+        const insertedRecolt = records[0];
+        const host = req.get('host');
+        const location = `http://${host}${req.url}/${insertedRecolt.id}`;
+        return res
+          .status(201)
+          .set('Location', location)
+          .json(insertedRecolt);
+      });
+    }
+  });
+});
+
+// modification d'une récolte en BDD
+app.put('/api/recoltes/:id', (req, res) => {
+  pool.query(`
+  UPDATE recolte SET ?
+  WHERE id=?
+  `, [req.body, req.params.id], (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message
+      })
+    } else if (results.length === 0) {
+      res.status(401).send('Cet id n’est pas en base de données.')
+    } else {
+      return pool.query('SELECT * FROM recolte WHERE id = ?', req.params.id, (err2, results2) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });
+        }
+        const modifiedRecolt = results2[0];
+        const host = req.get('host');
+        const location = `http://${host}${req.url}/${modifiedRecolt.id}`;
+        return res
+          .status(201)
+          .set('Location', location)
+          .json(modifiedRecolt);
+      });
     }
   });
 });
